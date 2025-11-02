7d3adbe392c24de3760b1b25dbc3370080583bb9
deepika <manideepika190@gamil.com>
2025-11-02 21:33:25 +0530

    how to print output in c++

---
+++ output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //without namespacing std syntax is std::cout<<
+    cout<<"Hello,This is my first cpp program!";
+    //print mutliple things in one line
+    cout<<"Hello"<<" "<<"World";
+    //2 ways to end a line in C++
+    cout<<"hello"<<endl<<"my first c++ prgm"; //1st way
+    cout<<"hello" <<"\n"<<"my first c++ prgm";//2nd way
+    
+
+
+    return 0;
+}
