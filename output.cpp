7c888d4467c210f017d740411df61d7914c1cf8b
deepika <manideepika190@gamil.com>
2025-11-02 21:38:48 +0530

    how to display in c++

--- output.cpp
+++ output.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
     //without namespacing std syntax is std::cout<<
-    cout<<"Hello,This is my first cpp program!";
+    cout<<"Hello,This is my first cpp program!"<<endl;
     //print mutliple things in one line
-    cout<<"Hello"<<" "<<"World";
+    cout<<"Hello "<<"World"<<endl;
     //2 ways to end a line in C++
-    cout<<"hello"<<endl<<"my first c++ prgm"; //1st way
+    cout<<"hello"<<endl<<"my first c++ prgm"<<endl; //1st way
     cout<<"hello" <<"\n"<<"my first c++ prgm";//2nd way
     
 
