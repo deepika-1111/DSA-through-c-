886aa75f8f61e101717cdf2f725c707273282062
deepika <manideepika190@gamil.com>
2025-11-03 20:48:17 +0530

    variables and datatyes in c++

---
+++ c++basics/variables_datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //variable declaration: datatype variable_name=value;
+    //int,float,double,bool,char are the primitive datatypes
+    //int variable 4bytes
+    int age=18;
+    //float/double variable float and double differ in size float=4bytes,double=8bytes
+    float price=11.9f;
+    //char variable 1byte 
+    char grade='A';
+    //bool variable 1byte(prints 0=false or 1=true)
+    bool ispass=true;
+    cout<<age<<endl<<grade<<endl<<ispass<<endl<<price<<endl;
+
+    //sizeof(): to find the size of any datatyes
+    cout<<"size of int:"<<sizeof(int);
+    return 0;
+}
