f520e3bb06bc8cc3379b4e7c0bed82fd77edd867
deepika <manideepika190@gamil.com>
2025-11-03 21:00:24 +0530

    type cast and type conversion

--- c++basics/type_cast_conversion.cpp
+++ c++basics/type_cast_conversion.cpp
@@ -3,5 +3,12 @@ using namespace std;
 int main()
 {
     //typeconversion->complier->small to big conversion
-    float 
+    float a=11.9f;
+    double b=a;
+    cout<<a<<endl<<b endl;
+    //typecast->manual->big to small conversion
+    int c=(int)a;
+    //round of to 11
+    cout<<c;
+    return 0;
 }
