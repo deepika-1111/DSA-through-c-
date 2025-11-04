abcd96f336e2f5a19ae971b596dafbead6e64cee
deepika <manideepika190@gamil.com>
2025-11-04 18:26:50 +0530

    operators in c++

--- c++basics/operators.cpp
+++ c++basics/operators.cpp
@@ -19,5 +19,11 @@ int main(){
     cout<<"a>b and a==b? : "<<(a>b && a==b)<<endl; //&& logical and
     cout<<"a>b or a==b? : "<<(a>b || a==b)<<endl; // || logical or
     cout<<"not a>b : "<<!(a>b)<<endl;// ! logical not
+    //unary operators 
+    b=a++; //post increment a++ ,pre increment ++a
+    int c=--a;//post decrement a--, post decrement --a
+    cout<<"b= "<<b<<endl; //first store in b then increment a=21
+    cout<<"c= "<<c<<endl;//first decrement a=20 then store in c
+    cout<<"a= "<<a<<endl;//increment and decrement = original
     return 0;
 }
